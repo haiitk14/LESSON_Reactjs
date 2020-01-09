33e63c4ddec922c51b159a18ac1c1758a45d7fc6
haidt <dotruonghaiitk14@gmail.com>
2020-01-09 17:35:53 +0700

    custom route

--- lesson15-react-route/src/App.js
+++ lesson15-react-route/src/App.js
@@ -1,9 +1,11 @@
 import React, { Component } from 'react';
 import './App.css';
-import { BrowserRouter as Router, Route, NavLink } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import Home from './components/Home';
 import About from './components/About';
 import Contact from './components/Contact';
+import NotFound from './components/NotFound';
+import Menu from './components/Menu';
 
 class App extends Component {
     render() {
@@ -11,24 +13,17 @@ class App extends Component {
             <Router>
                 <div className="App">
                     {/* MENU */}
-                    <nav className="navbar navbar-inverse">
-                        <ul className="nav navbar-nav">
-                            <li>
-                                <NavLink activeClassName="my-active" exact to="/" >Trang chủ</NavLink>
-                            </li>
-                            <li>
-                                <NavLink activeClassName="my-active" to="/about">Giới thiệu</NavLink>
-                            </li>
-                            <li>
-                                <NavLink activeClassName="my-active" to="/contact">Liên hệ</NavLink>
-                            </li>
-                        </ul>
-                    </nav>
-
+                    <Menu></Menu>
+                
                     {/* Nội dung */}
-                    <Route path="/" exact component={Home}></Route>
-                    <Route path="/about" component={About}></Route>
-                    <Route path="/contact" component={Contact}></Route>
+                    {/* Switch như witch case trong code */}
+                    <Switch>
+                        <Route path="/" exact component={Home}></Route>
+                        <Route path="/about" component={About}></Route>
+                        <Route path="/contact" component={Contact}></Route>
+                        <Route component={ NotFound }></Route>
+                    </Switch>
+                    
                 </div>
             </Router>
         );
---
+++ lesson15-react-route/src/components/Menu.js
@@ -0,0 +1,33 @@
+import React, { Component } from 'react';
+import { Route, Link } from 'react-router-dom';
+
+const MenuLink = ({ label, to, activeOnlyWhenExact }) => {
+    return (
+        <Route path={ to } exact={ activeOnlyWhenExact } children={ ({ match }) => {
+            var active = match ? 'active abc' : '';
+            return (
+                <li className={ `my-li ${ active }`}>
+                    <Link to={ to } className='my-link'>
+                        { label }
+                    </Link>
+                </li>
+            ) 
+        }} />
+    );
+}
+class Menu extends Component {
+    render() {
+        return (
+            <nav className="navbar navbar-default">
+                <ul className="nav navbar-nav">
+                    <MenuLink label="Trang chủ" to="/" activeOnlyWhenExact={true} />
+                    <MenuLink label="Giới thiệu" to="/about" activeOnlyWhenExact={false} />
+                    <MenuLink label="Liên hệ" to="/contact" activeOnlyWhenExact={false} />
+                </ul>
+            </nav>
+        );
+    }
+
+}
+
+export default Menu;
