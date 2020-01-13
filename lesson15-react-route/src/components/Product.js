7f59d95e96ff32613992ced8c262658984a143b1
haidt <dotruonghaiitk14@gmail.com>
2020-01-14 00:17:31 +0700

    get slug + reset

---
+++ lesson15-react-route/src/components/Product.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+
+class Product extends Component {
+    render() {
+        let {match} = this.props;
+        let name = match.params.slug;
+        console.log(name);
+
+        return (
+            <div>
+               Đây là chi tiết sản phẩm: { name }
+            </div>
+        );
+    }
+
+}
+
+export default Product;
---
+++ lesson15-react-route/src/components/Products.js
@@ -0,0 +1,63 @@
+import React, { Component } from 'react';
+import { NavLink, Route } from 'react-router-dom';
+import Product from './Product';
+
+class Products extends Component {
+    render() {
+        let products = [
+            {
+                id: 1,
+                slug: 'iphonex',
+                name: 'IP X',
+                price: 15000000
+            },
+            {
+                id: 2,
+                slug: 'iphone6',
+                name: 'IP 6',
+                price: 5000000
+            },
+            {
+                id: 3,
+                slug: 'iphone7plus',
+                name: 'IP 7 plus',
+                price: 10000000
+            }
+        ];
+        let {match} = this.props;
+        let url = match.url;
+
+        let result = products.map((item, index) => {
+            return (
+                <NavLink to={ `${url}/${item.slug}` } key={index}>
+                    <li  className="list-group-item">
+                        {item.name} - {item.price}
+                    </li>
+                </NavLink>
+
+            )
+        })
+        return (
+            <div className="container">
+                <h1>Danh sách sản phẩm</h1>
+
+                <div className="row">
+
+                    <ul className="list-group">
+                        {result}
+                    </ul>
+
+                </div>
+                
+                <div className="row">
+                    <Route path='/products/:slug' component={ Product }></Route>
+                </div>
+                
+
+            </div>
+        );
+    }
+
+}
+
+export default Products;
